f39be20db374d14264acddcc0623aed21dbfd4df
sri-varshan14 <mailmeatsri14@gmail.com>
2023-04-17 22:33:37 +0000

    [Fix]RandomBlob Canvas

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import './App.css'
-import './RandomBlob/Blob.css'
+import { useRef } from 'react';
 import RandomBlob from './RandomBlob/RandomBlob';
 
 function App() {
+    const windowWidth = useRef(window.innerWidth);
+    const windowHeight = useRef(window.innerHeight);
+
+    let width = windowWidth.current
+    let height = windowHeight.current
+
     var scheme: string[];
     scheme = ["#f5e0dc", "#f2cdcd", "#f5c2e7", "#cba6f7", "#f38ba8", "#eba0ac", "#fab387", "#f9e2af", "#a6e3a1", "#94e2d5", "#89dceb", "#74c7ec", "#89b4fa", "#b4befe"];
     return (
-        <div className='App w-full h-full bg-cbase'>
-            <RandomBlob num_of_blob={50} fill_color_scheme={scheme} />
+        <div className='App w-full h-full bg-cbase' id="App">
+            <RandomBlob num_of_blob={25} fill_color_scheme={scheme} canvas_width={width * 0.9} canvas_height={height * 5.8} />
         </div>
     );
 }
--- src/RandomBlob/RandomBlob.tsx
+++ src/RandomBlob/RandomBlob.tsx
@@ -1,4 +1,3 @@
-import './Blob.css'
 import './RandomNumberGenerator';
 import getRandomeNumBtw from './RandomNumberGenerator';
 import SquareBlob from './SquareBlob';
@@ -11,43 +10,43 @@ import CircleBlob from './CircleBlob';
 interface RandomBlobProp {
     num_of_blob: number
     fill_color_scheme: Array<string>
+    canvas_width: number
+    canvas_height: number
 }
 
 
 function RandomBlob(prop: RandomBlobProp) {
     const constraintsRef = useRef(null)
 
-    console.log(constraintsRef.current)
-    console.log(constraintsRef.current)
     let BlobArr: any[] = [];
     for (let i = 0; i < prop.num_of_blob; i++) {
         BlobArr.push(<SquareBlob
             width={getRandomeNumBtw(30, 90)}
             fill={prop.fill_color_scheme[getRandomeNumBtw(0, prop.fill_color_scheme.length)]}
-            x={getRandomeNumBtw(20, 1920)}
-            y={getRandomeNumBtw(30, 6000)}
+            x={getRandomeNumBtw(20, prop.canvas_width)}
+            y={getRandomeNumBtw(30, prop.canvas_height)}
             rotate={getRandomeNumBtw(-360, 360)}
             constraintsRef={constraintsRef} />)
         BlobArr.push(<TriangleBlob
             width={getRandomeNumBtw(30, 90)}
             fill={prop.fill_color_scheme[getRandomeNumBtw(0, prop.fill_color_scheme.length)]}
-            x={getRandomeNumBtw(20, 1920)}
-            y={getRandomeNumBtw(30, 6000)}
+            x={getRandomeNumBtw(20, prop.canvas_width)}
+            y={getRandomeNumBtw(30, prop.canvas_height)}
             rotate={getRandomeNumBtw(-360, 360)}
             constraintsRef={constraintsRef} />)
         BlobArr.push(<StickBlob
             width={getRandomeNumBtw(2, 4)}
             height={getRandomeNumBtw(80, 200)}
             fill={prop.fill_color_scheme[getRandomeNumBtw(0, prop.fill_color_scheme.length)]}
-            x={getRandomeNumBtw(20, 1920)}
-            y={getRandomeNumBtw(30, 6000)}
+            x={getRandomeNumBtw(20, prop.canvas_width)}
+            y={getRandomeNumBtw(30, prop.canvas_height)}
             rotate={getRandomeNumBtw(-360, 360)}
             constraintsRef={constraintsRef} />)
         BlobArr.push(<CircleBlob
             width={getRandomeNumBtw(30, 200)}
             fill={prop.fill_color_scheme[getRandomeNumBtw(0, prop.fill_color_scheme.length)]}
-            x={getRandomeNumBtw(20, 1920)}
-            y={getRandomeNumBtw(30, 6000)}
+            x={getRandomeNumBtw(20, prop.canvas_width)}
+            y={getRandomeNumBtw(30, prop.canvas_height)}
             rotate={getRandomeNumBtw(-360, 360)}
             constraintsRef={constraintsRef} />)
     }
