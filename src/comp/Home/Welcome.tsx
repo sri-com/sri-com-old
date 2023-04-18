95772ff2d2f3b1b4f431d2abf51406d0a3527b33
sri-varshan14 <mailmeatsri14@gmail.com>
2023-04-18 10:43:15 +0000

    [Feat] Completed Welcome Section

--- src/comp/Home/Welcome.tsx
+++ src/comp/Home/Welcome.tsx
@@ -1,7 +1,14 @@
+import './Welcome.css'
+
 function Welcome() {
     return (
-        <div className="WelcomeContainer float-left w-[60%] top-14 h-full">
-
+        <div className="WelcomeContainer float-left w-[60%] top-14 h-full flex flex-col justify-center items-start pl-80">
+            <p className="WelcomeText text-5xl mt-[-250px] text-ctext">Hi, I'm</p>
+            <p className="WelcomeText text-6xl" id="NameText">Srivarshan</p>
+            <p className="WelcomeText text-4xl pt-4 text-ctext">Computer Science Student</p>
+            <p className="WelcomeTextPara font-jetbrains text-ctext pt-7">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam varius urna id orci pharetra, non rhoncus est consectetur. Fusce libero massa, placerat vitae ipsum in, scelerisque vulputate elit. Proin tempor, nunc in ornare dictum, sem libero blandit tellus, a mattis quam dui nec mauris. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Ut a interdum tortor. Integer accumsan, mauris eget blandit pellentesque, dolor est vestibulum ex
+            </p>
         </div>
     )
 }
