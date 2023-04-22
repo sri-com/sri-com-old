10af38426e947ac414820ac530327988e8d57c44
sri-varshan14 <mailmeatsri14@gmail.com>
2023-04-22 22:07:31 +0000

    [FIX] Responsiveness of Home section complete

--- src/comp/Home/ProfilePic.tsx
+++ src/comp/Home/ProfilePic.tsx
@@ -2,8 +2,8 @@ import pic from './../../assets/profile.png'
 
 function ProfilePic() {
     return (
-        <div className='ImageContainer w-[40%] float-right absolute top-[10%] right-7'>
-            <img src={pic} className="h-[80%] min-h-max" />
+        <div className='ImageContainer flex items-center justify-center'>
+            <img src={pic} className="" />
         </div>
     )
 }
