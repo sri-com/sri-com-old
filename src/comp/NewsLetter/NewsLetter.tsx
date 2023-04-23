28539c415c9112704071f9f3f73ea5e348e8fbab
sri-varshan14 <mailmeatsri14@gmail.com>
2023-04-23 16:27:59 +0000

    [FIX] Tried to fix NewsLetter layout Good for >1300px

--- src/comp/NewsLetter/NewsLetter.tsx
+++ src/comp/NewsLetter/NewsLetter.tsx
@@ -1,11 +1,11 @@
 import SubscribeTextBox from './SubscribeTextBox';
 import MailIdSubmitBox from './MailIdSubmitBox';
-import './NewsLetter.css'
+import "./NewsLetter.css";
 
 function NewsLetter() {
     return (
-        <div className='bg-transparent flex justify-center items-center absolute top-[4525px] w-[100%] h-80'>
-            <div className='bg-c-text w-[60%] h-[90%] rounded-[60px] flex items-center justify-center'>
+        <div className='NewsLetter bg-transparent flex justify-center items-center absolute top-[calc(550vh_-_75%)] w-[100%] h-80'>
+            <div className='NewsLetterContainer bg-c-text w-[60%] h-[90%] rounded-[60px] flex items-center justify-center'>
                 <SubscribeTextBox />
                 <MailIdSubmitBox />
             </div>
