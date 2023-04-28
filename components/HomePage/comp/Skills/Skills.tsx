f71a6f55e408c409338c36cdce44421bf842dc4f
sri-varshan14 <mailmeatsri14@gmail.com>
2023-04-28 00:07:27 +0000

    Fixed about skill section layout

--- components/HomePage/comp/Skills/Skills.tsx
+++ components/HomePage/comp/Skills/Skills.tsx
@@ -1,13 +1,11 @@
 function Skills() {
     return (
-        <section className='SkillsContainer w-screen h-screen flex items-center justify-around absolute top-[calc(200vh)]'>
-            <div className='SkillsBox flex flex-col justify-center items-center h-full w-[30%]'>
+        <section className='SkillsContainer w-full h-[80%] flex items-center justify-around absolute top-[calc(200vh)] px-6'>
+            <div className='SkillsBox flex flex-col justify-center items-center w-[30%] '>
                 <p className='font-jetbrains text-4xl text-c-text '>Skills</p>
                 <p className='font-jetbrains text-9xl text-c-overlay0'>Skills</p>
             </div>
-            <div className='SkillsContent flex items-center justify-center h-full w-[70%] '>
-                <div className='SkillsMotionDiv bg-c-crust opacity-90 rounded-3xl border-4 border-c-text'>
-                </div>
+            <div className='SkillsContent flex items-center justify-center h-full w-[70%] bg-c-crust opacity-90 border-4 border-c-text rounded-xl px-6'>
             </div>
         </section>
     )
