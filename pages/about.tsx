c00abf877c214256dd9f3b64a5fea0a06078d2d7
sri-varshan14 <mailmeatsri14@gmail.com>
2023-05-09 23:37:53 +0000

    Removed pages/about.tsx and added routes about and blogs

--- pages/about.tsx
+++
@@ -1,3 +0,0 @@
-export default function Home() {
-    return <h1>Enga da inga irrundha website</h1>
-}
---
+++ pages/blogs/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Blogs() {
+    return (
+        <>
+            <Head>
+                <title>Blog Post</title>
+            </Head>
+            <main className="w-[100vw] h-[100vh]">
+
+            </main>
+        </>
+    )
+}
