6a558d84465dbb8ca509cede27976d16a8ef30f2
sri-varshan14 <mailmeatsri14@gmail.com>
2023-05-09 00:35:00 +0000

    Removed the blog post route and completed index page serving using actix-web

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,42 +1,20 @@
-use rocket::{get, routes, launch};
-use rocket::fs::NamedFile;
-use rocket::form::FromForm;
-use std::path::{Path, PathBuf};
+use actix_web::{get, HttpServer, App, Responder }; 
+use actix_files::NamedFile;
 
 #[get("/")]
-async fn index() -> Option<NamedFile> {
-    NamedFile::open("./../.next/server/pages/index.html").await.ok()
+async fn index() -> impl Responder {
+    NamedFile::open_async("./../.next/server/pages/index.html").await
 }
 
-#[get("/<file..>")]
-async fn files(file: PathBuf) -> Option<NamedFile> {
-    NamedFile::open(Path::new(&format!("./../.next/{}",file.to_str().unwrap()))).await.ok()
-}
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
 
-#[derive(FromForm)]
-struct ImageParams {
-    url: Option<String>,
-    w: Option<u32>,
-    q: Option<u8>,
-}
-
-#[get("/image?<image_params..>")]
-async fn serve_image(image_params: ImageParams) -> Option<NamedFile> {
-    let url = image_params.url.expect("Missing 'url' parameter");
-    let w = image_params.w.unwrap_or(640);
-    let q = image_params.q.unwrap_or(75);
-
-    println!("{}-{}-{}",&url[2..],w,q);
-
-    // Serve the file as a response.
-    NamedFile::open(Path::new(&format!("./../.{}",&url[2..]))).await.ok()
-}
-
-#[launch]
-fn rocket() -> _ {
-    rocket::build()
-        .mount("/", routes![index])
-        .mount("/_next", routes![serve_image])
-        .mount("/_next", routes![files])
-        .mount("/_next", routes![files])
+    HttpServer::new(|| {
+        App::new()
+            .service(index)
+    })
+    .bind("127.0.0.1:8080")?
+    .workers(12)
+    .run()
+    .await
 }
