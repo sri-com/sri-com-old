c9e0f06c586bd1f0392f759f72ee006973db2957
sri-varshan14 <mailmeatsri14@gmail.com>
2023-05-21 14:24:04 +0530

    Added /submit route

--- components/HomePage/comp/NewsLetter/MailIdSubmitBox.tsx
+++ components/HomePage/comp/NewsLetter/MailIdSubmitBox.tsx
@@ -1,12 +1,54 @@
+import handler from "@/pages/api/hello";
+import { useState } from "react";
+
 function MailIdSubmitBox() {
+
+    const [formData, setFormData] = useState({
+        email: '',
+    });
+
+    const handleInputChange = (event: any) => {
+        setFormData({
+            ...formData,
+            [event.target.name]: event.target.value,
+        });
+    };
+
+    const handleSubmit = (event: any) => {
+        event.preventDefault();
+
+        // Convert form data to JSON
+        const jsonData = JSON.stringify(formData);
+
+        // Send POST request
+        fetch('/submit', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: jsonData,
+        })
+            .then((response) => {
+                if (response.ok) {
+                    // Redirect to another route after successful submission
+                    window.location.href = '/';
+                } else {
+                    // Handle error
+                    console.error('Error:', response.status);
+                }
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+    };
 
     return (
         <div className="w-[50%] ">
             <div className="MailIdSubmitContainer border-c-base border-[3px] rounded-[4rem] items-center p-[5%] justify-around">
-                <form className="FormBox flex justify-around items-center place-items-center w-[100%]" action="/submit" method="POST">
+                <form className="FormBox flex justify-around items-center place-items-center w-[100%]" onSubmit={handleSubmit}>
                     <label htmlFor="email" ></label>
-                    <input className="InputBox bg-c-text m-1 outline-none w-[90%]" placeholder="Email Address" type="email" id="email" name="email" required />
-                    <button className="bg-c-base h-9 text-xl px-3 font-semibold rounded-2xl text-center text-c-text font-jetbrains flex items-center justify-center w-[10%]">&gt;</button>
+                    <input className="InputBox bg-c-text m-1 outline-none w-[90%]" placeholder="Email Address" type="email" id="email" name="email" value={formData.email} onChange={handleInputChange} required />
+                    <button className="bg-c-base h-9 text-xl px-3 font-semibold rounded-2xl text-center text-c-text font-jetbrains flex items-center justify-center w-[10%]" type="submit">&gt;</button>
                 </form>
 
             </div>
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,8 +1,5 @@
-use actix_web::{get, web, Responder, HttpServer, App};
+use actix_web::{get, post, web, Responder, HttpResponse, HttpServer, App};
 use actix_files::NamedFile;
-use actix_web::middleware::Logger;
-use env_logger::Env;
-use tracing_actix_web::TracingLogger;
 use serde::Deserialize;
 
 #[get("/")]
@@ -13,16 +10,12 @@ async fn index() -> impl Responder {
 #[get("/_next/{filename:.*}")]
 async fn other_files(filename: web::Path<String>) -> impl Responder { 
     let file_path = format!("../.next/{}",filename.to_string());
-    println!("{}",file_path);
-
     NamedFile::open_async(file_path).await
 }
 
 #[derive(Deserialize,Debug)]
 struct ImageRequest {
     url: String,
-    w: Option<u32>,
-    q: Option<u8>,
 }
 
 #[get["_next/image"]]
@@ -33,6 +26,17 @@ async fn image_req(image_request: web::Query<ImageRequest>) -> impl Responder {
 }
 
 
+#[derive(Deserialize,Debug)]
+struct FormData {
+    email: String,
+}
+
+#[post("/submit")]
+async fn get_mailid(form_data: web::Json<FormData>) -> impl Responder {
+    println!("{:#?}",form_data.email);
+    HttpResponse::Ok()
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
@@ -40,6 +44,7 @@ async fn main() -> std::io::Result<()> {
             .service(index)
             .service(image_req)
             .service(other_files)
+            .service(get_mailid)
     })
     .workers(12)
     .bind("127.0.0.1:8000")?
